61c7a42322f96f729b67075cf94c5b382afa0ff2
CALISTA TAN <254771N@mymail.nyp.edu.sg>
2026-07-11 16:06:35 +0800

    Integrate Board Paper and Proposal backend

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,6 +6,7 @@ const tenderRoutes = require('./tenderRoutes');
 const evaluationRoutes = require('./evaluationRoutes');
 const boardPaperRoutes = require('./boardPaperRoutes');
 const clarificationRoutes = require('./clarificationRoutes');
+const proposalRoutes = require('./proposalRoutes');
 
 router.use('/auth', authRoutes);
 router.use('/dashboard', dashboardRoutes);
@@ -13,5 +14,6 @@ router.use('/tenders', tenderRoutes);
 router.use('/evaluations', evaluationRoutes);
 router.use('/boardpapers', boardPaperRoutes);
 router.use('/clarifications', clarificationRoutes);
+router.use('/proposals', proposalRoutes);
 
 module.exports = router;
